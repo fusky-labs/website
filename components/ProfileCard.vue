6ac7f2550f04a4aaf2233170e3a17de38d028531
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-03-27 00:02:39 +0800

    Added ProfileCard component

---
+++ components/ProfileCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ name?: string }>(), { name: 'Yes' })
+</script>
+
+<template>
+  <div id="profile-card">
+    <img :src="`https://github.com/${name}.png`">
+    <h2>{{ name }}</h2>
+    <p><slot></slot></p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#profile-card {
+  background: white;
+  padding: 2.65ex;
+  text-align: center;
+  box-shadow: 2px 0 10px rgba(black, 22%);
+  width: 20%;
+  border-radius: 7px;
+}
+
+img {
+  width: clamp(127px, 100%, 150px);
+  margin: 0 auto;
+  border-radius: 50%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <script>
-export default {layout: 'default'}
+import ProfileCard from "~~/components/ProfileCard.vue";
+export default { layout: "default", components: { ProfileCard } }
 </script>
 
 <template>
   <div>
-    <h2>welcome!</h2>
+    <h2>Members</h2>
+    <div class="item-flex">
+      <ProfileCard name="skepfusky">Self-taught front-end developer, musician, filmmaker, jack of all trades</ProfileCard>
+      <ProfileCard name="thatITfox">Back-end developer, into AI and world domination</ProfileCard>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.item-flex {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1.25rem;
+  gap: 1rem;
+}
+</style>
