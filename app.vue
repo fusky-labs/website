63554f0d782e07dba000d35aba11e6a342907764
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-06-30 10:22:25 +0800

    Added meta tags

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+const title = "OpenFurs";
+const description =
+  "Hi! We are OpenFurs! We are just two idiots from Southeast Asia making stuff with web and AI technologies";
+
+useHead({
+  title: title,
+  meta: [
+    { name: "description", content: description },
+    { property: "og:type", content: "website" },
+    { property: "og:title", content: title },
+    { property: "og:description", content: description },
+    { name: "twitter:title", content: title },
+    { name: "twitter:description", content: description },
+    { name: "twitter:card", content: "summary-large-image" },
+  ],
+});
+</script>
+
 <template>
   <div>
     <HeaderHero />
